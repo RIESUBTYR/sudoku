e78c7ffc40ffa2251946d4ea34d8c912b6dae07a
abuziyaadawajeeh <mswaji09@gmail.com>
2020-11-01 11:56:43 +0530

    comments added

--- src/features/board/board.js
+++ src/features/board/board.js
@@ -5,8 +5,7 @@ import {useSelector} from "react-redux";
 
 export default function Board() {
 
-    const [correctcount, increasecount] = useState(0);
-
+    
     var initialarray = [
         [[7,1],[9,1],[2,1],[4,0],[8,1],[3,0],[6,1],[5,0],[1,0]],
         [[8,0],[6,0],[5,1],[9,0],[7,0],[1,1],[4,0],[3,1],[2,1]],
@@ -18,17 +17,13 @@ export default function Board() {
         [[3,0],[4,1],[7,1],[5,1],[2,0],[8,1],[1,0],[6,0],[9,1]],
         [[1,1],[2,1],[6,0],[3,1],[9,1],[7,1],[5,0],[8,1],[4,0]]
     ]
-
-    const incrementor = ivalue => {
-        increasecount(correctcount + ivalue);
-    }
-
-    const issolved = useSelector(state => state.board.issolved);
+        
+    const issolved = useSelector(state => state.board.issolved); //selecting data from redux store
 
     useEffect(() => {
         if(issolved)
             alert("Congrats! You have solved it!");
-    } );
+    } ); //useEffect hook 
 
     var allcells = [], key=0;
     for(var i=1;i<10;i++)
@@ -38,15 +33,16 @@ export default function Board() {
             bottom : 0
         };
         if(i==3 || i==6) //this logic is used to create borders for all the nine 3x3 squares
-             borderinfo.bottom = 1;
+             borderinfo.bottom = 1; //cells which has 3 or 6 as index values should have right/bottom border
         if(j==3 || j==6)    
             borderinfo.right = 1;
-        allcells.push(<Cell cellinfo = {initialarray[i-1][j-1]} incrementor={incrementor} borderinfo={borderinfo} key={key++} i={i} j={j} />);
+        allcells.push(<Cell cellinfo = {initialarray[i-1][j-1]} borderinfo={borderinfo} key={key++} i={i} j={j} />); //list rendering in React
     }
 
     
 
     return (
+        //learn about css/scss modules and how to use them in react
         <div className={Boardstyles.board} >
             {allcells}
         </div>
--- src/features/board/cell.js
+++ src/features/board/cell.js
@@ -1,27 +1,29 @@
 import React, {useState, useEffect} from 'react'
 import Cellstyles from "./cell.module.scss"; //'scss module', not a normal scss file
-import {useSelector, useDispatch} from 'react-redux'
-import {highlighter, countincrement} from "./boardSlice";
+import {useSelector, useDispatch} from 'react-redux' //using Redux inside react components
+import {highlighter, countincrement} from "./boardSlice"; //importing 'redux action creators' corresponding to the reducers in boardSlice file. 
 
 export default function Cell(props) {
-    const {i,j} = props;
+    const {i,j} = props; //destructuring  object
     const [value, visibility] = props.cellinfo; //destructuring array
-    const cellid = (i*10)+j;
+    const cellid = (i*10)+j; //a two digit integer
     var rightborder, bottomborder;
-    const {right, bottom} = props.borderinfo; //'destructuring from props'
+    const {right, bottom} = props.borderinfo; //destructuring
     rightborder = Cellstyles.rightborder;
     bottomborder = Cellstyles.bottomborder;
+    var elementtorender;
+
 
     
     const dispatch = useDispatch();
      const highlightcell = () => {
-        dispatch(highlighter({cellid : cellid}))
+        dispatch(highlighter({cellid : cellid})) //dispatching redux actions . The highligter() function we imported will return a redux action. The dispatch() function will then dispatch that action. 
     }
 
     const hightlightedcell = useSelector((state) => state.board.highlightedcell);
     const inputnum = useSelector(state => state.board.inputnum);
 
-    const [flag, setflag] = useState(0);
+    const [flag, setflag] = useState(0); //using this flag to increment/decrement count exactly once for correct/wrong input 
     
     
     useEffect(() => {
@@ -29,7 +31,7 @@ export default function Cell(props) {
         if(hightlightedcell == cellid){
             element.classList.add(Cellstyles.highlight);
         }
-        else  element.classList.remove(Cellstyles.highlight);
+        else  element.classList.remove(Cellstyles.highlight); //when the highlighted cell id changes inside the store, all cells will rerender. However, only the cell whose id equals that id inside state can highlight itself. 
     })
     useEffect(() => {
         var element = document.getElementById(cellid);
@@ -46,17 +48,13 @@ export default function Cell(props) {
         }
     })
     
-    const [tempvalue, settempvalue] = useState(null);
 
-    var elementtorender;
     if(visibility)
         elementtorender =  <div id={cellid} className ={`${Cellstyles.cell} ${right ? rightborder : "" } ${bottom ? bottomborder : ""} ${Cellstyles.visible} `} >
             {value}
         </div>
     else
-        elementtorender =  <div id={cellid} className ={`${Cellstyles.cell} ${right ? rightborder : "" } ${bottom ? bottomborder : ""} ${Cellstyles.hidden} `} onClick={highlightcell} >
-            {tempvalue}
-        </div>
+        elementtorender =  <div id={cellid} className ={`${Cellstyles.cell} ${right ? rightborder : "" } ${bottom ? bottomborder : ""} ${Cellstyles.hidden} `} onClick={highlightcell} ></div>
 
    
 
